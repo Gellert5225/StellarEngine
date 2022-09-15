c9cad2e69126a741937b82ccd98db73e4df7c1ba
gellert5225 <gellert.li1995@gmail.com>
2022-09-14 20:27:40 -0700

    fix imgui

--- Stellar/vendor/imgui/imconfig.h
+++ Stellar/vendor/imgui/imconfig.h
@@ -23,7 +23,18 @@
 // Using Dear ImGui via a shared library is not recommended, because of function call overhead and because we don't guarantee backward nor forward ABI compatibility.
 // DLL users: heaps and globals are not shared across DLL boundaries! You will need to call SetCurrentContext() + SetAllocatorFunctions()
 // for each static/DLL boundary you are calling from. Read "Context and Memory Allocators" section of imgui.cpp for more details.
-#define IMGUI_API __declspec( dllexport )
+#if defined(_WIN64)
+    #define IMGUI_API __declspec(dllexport)
+    #define DEBUG_BREAK __debugbreak()
+#elif defined(__GNUC__)
+    #define IMGUI_API __attribute__((visibility("default")))
+    #define DEBUG_BREAK raise(SIGTRAP)
+#elif defiend(__APPLE__)
+    #define IMGUI_API
+    #include <signal.h>
+    #define DEBUG_BREAK rais(SIGTRAP)
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
 //#define IMGUI_API __declspec( dllimport )
 
 //---- Don't define obsolete functions/enums/behaviors. Consider enabling from time to time after updating to avoid using soon-to-be obsolete function/names.
