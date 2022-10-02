9e6d6e898734f5bfc56156189638d3ae71472b81
Gellert <gellert.li1995@gmail.com>
2022-10-02 05:37:49 -0700

    minor fix

--- Stellar/src/Stellar/Platform/Vulkan/ImGui/VulkanImGuiLayer.cpp
+++ Stellar/src/Stellar/Platform/Vulkan/ImGui/VulkanImGuiLayer.cpp
@@ -125,10 +125,10 @@ namespace Stellar {
     void VulkanImGuiLayer::end() const {
         ImGuiIO& io = ImGui::GetIO();
         Application& app = Application::Get();
-        // io.DisplaySize = ImVec2(app.getWindow().getWidth(), app.getWindow().getHeight());
+        io.DisplaySize = ImVec2(app.getWindow().getWidth(), app.getWindow().getHeight());
 
-        // auto time = (float)glfwGetTime();
-        // io.DeltaTime = m_Time > 0.0 ? (time - m_Time) : (1.0f / 60.0f);
+        auto time = (float)glfwGetTime();
+        io.DeltaTime = m_Time > 0.0 ? (time - m_Time) : (1.0f / 60.0f);
 
         ImGui::Render();
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
